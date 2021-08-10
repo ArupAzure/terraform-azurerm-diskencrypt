8b1c3b5e65b78e8f9634f913451ba500050898f3
Mallikarjuna Reddy Tunga <mallikarjunareddy.tunga@gmail.com>
2021-08-10 10:34:49 +0300

    Update config.tf

--- rhel-vm/config.tf
+++ rhel-vm/config.tf
@@ -3,12 +3,18 @@ provider "azurerm" {
   //subscription_id = "12345678-1234-1234-1234-12345678"
 }
 
-/*terraform {
-  backend "azurerm" {
+terraform {
+    required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  /*backend "azurerm" {
     //subscription_id      = "12345678-1234-1234-1234-12345678"
     resource_group_name  = "terrafrom-rg"
     storage_account_name = "vmtfbackend01"
     container_name       = "abrtfkeystore"
     key                  = "windows-vm-key"
-  }
-}*/
+  }*/
+}
